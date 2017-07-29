c6840c7b3be3c953cfbb82201904d24bd21177a1
Niels de Blaauw <niels.de.blaauw@gmail.com>
2017-07-29 13:08:25 +0200

    Show not implemented warning only once per field

--- src/FieldTypes/NotImplementedField.php
+++ src/FieldTypes/NotImplementedField.php
@@ -6,7 +6,7 @@ use NDB\QualityControl\PostType;
 class NotImplementedField extends Base implements iFieldType{
   public function __construct(array $field, PostType $post_type){
     parent::__construct($field, $post_type);
-    \NDB\QualityControl\Command::$warnings[] = sprintf('Field %s of type %s is not yet implemented', $field['name'], $field['type']);
+    \NDB\QualityControl\Command::$warnings[$this->field['key']] = sprintf('Field %s of type %s is not yet implemented', $field['name'], $field['type']);
   }
 
   public function generate(int $post_id){}
