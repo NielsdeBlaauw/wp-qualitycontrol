a41ec2aa2c40fe9904169259c4fd969a2773fc1e
Niels de Blaauw <niels.de.blaauw@gmail.com>
2017-07-29 16:13:50 +0200

    Allows for selecting multiple results in post_objects and relation fields

--- src/FieldTypes/PostObject.php
+++ src/FieldTypes/PostObject.php
@@ -3,16 +3,33 @@
 namespace NDB\QualityControl\FieldTypes;
 
 class PostObject extends Base implements iFieldType{
+  protected $min_field = 'min';
+  protected $max_field = 'max';
+  protected $max_default = 50;
+  protected $ids = array();
+
   public function generate(int $post_id){
     $field_object = new \acf_field_post_object();
     $results = $field_object->get_ajax_query(array(
       'field_key'=>$this->field['key']
     ));
-    $chosenItem = array_rand($results['results']);
-    if(isset($results['results'][$chosenItem]['children'])){
-      $chosenChild = array_rand($results['results'][$chosenItem]['children']);
-      return $results['results'][$chosenItem]['children'][$chosenChild]['id'];
+    $ids = array();
+    foreach($results['results'] as $result){
+      if(isset($result['children'])){
+        foreach($result['children'] as $child){
+          $ids[] = $child['id'];
+        }
+      }
+      if(isset($result['id'])){
+        $ids[] = $result['id'];
+      }
     }
-    return $results['results'][$chosenItem]['id'];
+    $this->ids = $ids;
+    return $this->faker->randomElements($ids, $this->faker->numberBetween($this->get_min(), $this->get_max()));
+  }
+
+  public function get_max() : int{
+    $max_to_select = parent::get_max();
+    return min(count($this->ids), $max_to_select);
   }
 }
