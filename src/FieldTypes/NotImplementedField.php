fa816bd72acfcecd357644112db18b5123cad54f
Niels de Blaauw <niels@level-level.com>
2018-06-06 08:41:39 +0000

    Use old method of reporting warnings

--- src/FieldTypes/NotImplementedField.php
+++ src/FieldTypes/NotImplementedField.php
@@ -6,10 +6,7 @@ use NDB\QualityControl\iContext;
 class NotImplementedField extends Base implements iFieldType{
   public function __construct(\NDB\QualityControl\FieldDefinitions\FieldDefinition $field, iContext $context){
     parent::__construct($field, $context);
-    do_action('\ndb\qualitycontrol\warning', array(
-      'name'=>$this->field->get_key().'_implemented', 
-      'description'=>sprintf('Field %s of type %s is not yet implemented', $field->get_name(), $field->get_name()))
-    );
+    \NDB\QualityControl\Command::$warnings[$this->field->get_key().'_implemented'] = sprintf('Field %s of type %s is not yet implemented', $field->get_name(), $field->get_type());
   }
 
   public function generate($post_id){}
